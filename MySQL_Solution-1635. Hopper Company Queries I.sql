c2e5c40298bd007e0f4fc5a1d79734916936721f
Yunyi Wang <56772407+StanMillerW@users.noreply.github.com>
2020-11-02 16:21:12 -0500

    Update MySQL_Solution-1635. Hopper Company Queries I.sql

--- MySQL_Solution-1635. Hopper Company Queries I.sql
+++ MySQL_Solution-1635. Hopper Company Queries I.sql
@@ -16,7 +16,8 @@ GROUP BY  month),
 accepted_rides AS
 (SELECT DISTINCT month, COUNT(a.ride_id) AS accepted_rides
 FROM
-    AcceptedRides a INNER JOIN (SELECT *, MONTH(requested_at) AS request_month FROM Rides WHERE requested_at                                     REGEXP '2020' ORDER BY requested_at )r ON a.ride_id = r.ride_id 
+    AcceptedRides a INNER JOIN (SELECT *, MONTH(requested_at) AS request_month FROM Rides WHERE requested_at 
+				REGEXP '2020' ORDER BY requested_at )r ON a.ride_id = r.ride_id 
                     RIGHT JOIN temp_month ON r.request_month= temp_month.month
 GROUP BY month)
 # merge the two tables above to generate the results
